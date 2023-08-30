f4b7a9ddb37528b514f35e96a32e3b405734588b
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-08-30 12:43:50 -0700

    Uploading C "sayhello" files

---
+++ homework/homework01/sayhello.c
@@ -0,0 +1,13 @@
+ /* Aidan Esposito */
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+
+   int main( int argc, char * argv[] ) {
+      char namePrint[25];
+      char input[25];
+      printf("\n\n Please Enter your name\n\n");
+      gets(input);
+      printf("\n\n Hello, %s\n\n", input);
+
+   }
