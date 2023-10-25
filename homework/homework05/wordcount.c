ac330adddd77fe57ae3e7b4bfa972394f629b545
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-10-25 10:59:41 -0700

    Updated WordCount with comments

--- homework/homework05/wordcount.c
+++ homework/homework05/wordcount.c
@@ -6,23 +6,26 @@
 #include <string.h>
 #include <ctype.h>
 
+
+//Main used to count number of words in a given text file or directory path
 int main( int argc, char * argv[] ) {
 
-    int wordcount = 0;
-    int wordspace = 1;
-    int c;
+    int wordcount = 0; //variable to store wordcount
+    int wordspace = 1; //variable to help check for spaces
+    int c; //variable to compare to open file
 
+        //Checks for correct number of arguments
         if( argc != 2) {
             printf("\nNot Acceptable Argument, only use two arguments, one for the program and one for the file you want to count the words of.\n");
             return 1;
         }
         else{
             FILE* in = fopen(argv[1], "r");
-            if( !in){
+            if( !in){ //checks if the file exists on your computer
                 printf("File %s not found \n", argv[1]);
                 return 2;
             }
-            while((c = fgetc(in)) != EOF){
+            while((c = fgetc(in)) != EOF){ //checks for whitespace in the file and updates word count
                 if(isspace(c)){
                     wordspace = 1;
                 }
