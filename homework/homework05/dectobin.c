412045525f35d7d24d4e4ee18fba83dae9285696
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-10-25 12:05:46 -0700

    Uploaded Completed dectobin.c

--- homework/homework05/dectobin.c
+++ homework/homework05/dectobin.c
@@ -1,36 +1,37 @@
- //Aidan Esposito / Juan Ballesteros
+//Aidan Esposito / Juan Ballesteros
 
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //function that uses binary and remainder variables to convert the enter decimal number
 //to a 32 bit binary number.
-void DecimalToBinary(int decimal){  //Accounts if entered number is 0 or 1.
+void DecimalToBinary(int decimal){  //Sets original decimal number to 0
     if(decimal == 0){
-        printf("0x0");
+        printf("00000000000000000000000000000000");
+        return;
+    }
+    //checks if decimal number entered is in 32 bit range
+    if(decimal < 0 || decimal > 4294967296){
+        printf("numbers entered are not acceptable with a 32 bit binary number");
         return;
     }
 
-    int binary[32]; //account for 32 bits
-    int remainder = 0;
+    char binary[32] = {"00000000000000000000000000000000"}; //account for 32 bits
 
-    while(decimal > 0){
+    for(int i = 31; i >= 0; i--){ //converts decimal number to binary number
+        if(decimal % 2){
+            binary[i] = '1';
+        }
         decimal = decimal /2;
-        binary[remainder] = decimal % 2;
-        remainder ++;
     }
-
-    for(int i = remainder -1; i >= 0; i--){
-        printf("%d\n", binary[i]);
-    }
-
+    printf("%s\n", binary);
 }
 
 //Used to handle entered number by user and print the resulting binary number.
 int main() {
 
-    int enteredDecimalNumber;
+    int enteredDecimalNumber; //checks for entered decimal number in arguments
 
     printf("Please enter a decimal number to be converted to binary");
     scanf( "%d", &enteredDecimalNumber);
