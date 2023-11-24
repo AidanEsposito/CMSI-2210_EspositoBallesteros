20610eea2c5ce9767a0fe210ab8db5d9efdbcbb6
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-11-24 08:46:41 -0800

    Completed Problem 4

---
+++ homework/homework06/whichEndIsUp.c
@@ -0,0 +1,22 @@
+//Aidan Esposito / Juan Ballesteros
+
+#include <stdio.h>
+
+int main(){
+
+    //Number to check byte order
+    int num = 1;
+
+    //char used to check endian order with num value
+    char *endian_check = (char *)&num;
+
+    //If the first byte is 1
+    if(*endian_check == 1){
+        printf("Your system is Little Endian\n");
+    }
+    else{
+        printf("Your system is Big Endian\n");
+    }
+
+return 0;
+}
