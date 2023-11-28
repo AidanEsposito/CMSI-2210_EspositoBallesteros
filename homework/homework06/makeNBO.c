f0aac1c200aa2a5a6e3f82c05aea69833e819dad
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-11-27 20:59:50 -0800

    Delete homework/homework06/makeNBO.c

--- homework/homework06/makeNBO.c
+++
@@ -1,9 +0,0 @@
-//Aidan Esposito / Juan Ballesteros
-
-#include <stdint.h>
-
-// Function that converts the entered argument into Network Byte Order if necessary.
-// Returns the result to determine different bytes.
-uint32_t makeNBO(uint32_t num) {
-    return ((num >> 24) & 0xFF) | ((num >> 8) & 0xFF00) | ((num << 8) & 0xFF0000) | ((num << 24) & 0xFF000000);
-}
