bfd31c61e5f7eda48166449b78e8a9cc23613876
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-11-28 09:22:25 -0800

    Updated C file

--- homework/homework06/makeNBOC.c
+++ homework/homework06/makeNBOC.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdint.h>
 
-//makeNBO function to check byte order and print new number
-uint32_t makeNBO(uint32_t num) {
-    uint32_t result; //unit to print out 32 bit NBO number
-    uint8_t *bytePtr = (uint8_t*)&num; //pointer to compare endians
+//function that converts number given in argument to NBO format.
+uint32_t makeNBO(uint32_t num) { 
+    uint32_t result; //final number to be printed after conversion
+    uint8_t *bytePtr = (uint8_t*)&num; //pointer to allow for conversion of bits
 
-    if (*bytePtr == 1) { // If the system is little-endian, print byte order
+    if (*bytePtr == 1) { //converting each bits into opposite order if computer is little endian
         result = ((num & 0xFF000000) >> 24) |
                  ((num & 0x00FF0000) >> 8) |
                  ((num & 0x0000FF00) << 8) |
                  ((num & 0x000000FF) << 24);
     } else {
-        // If the system is big-endian just print number
+        //if big endian
         result = num;
     }
 
