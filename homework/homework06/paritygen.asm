12ea570916481a0c9bc7ebd04ffcc15474dd5e9f
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-11-28 19:05:21 -0800

    Uploaded Problem 3

---
+++ homework/homework06/paritygen.asm
@@ -0,0 +1,44 @@
+; Aidan Esposito / Juan Ballesteros
+
+section .text
+    global _main
+    extern _printf ; printf function from C to show parity bit
+
+_main:
+    mov eax, input_byte ; load input_byte into eax
+
+top:
+    test eax, 1 ; check if current value in byte is 1
+    jnz odd_parity ; if 1, go to odd parity
+
+even_parity:
+
+    ; print 0 for even parity
+    push '0'
+    push format
+    call _printf
+    add esp, 8      
+    jmp exit
+
+odd_parity:
+
+    ; print 1 for odd parity
+    push '1'
+    push format
+    call _printf
+    add esp, 8  
+
+    ; shift right to check next bit
+    shr eax, 1
+    jnz top  
+    jmp exit
+
+exit:
+    ; exit the program
+    mov eax, 0
+    ret
+
+section .data
+
+    input_byte db 103 ; byte used to test parity (103) 
+    format db "Parity Bit: %c", 10, 0 ; used to print out the end parity bit
