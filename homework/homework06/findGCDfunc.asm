cf226b3812fc42e7d249365b8b94f82b45f7fabe
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2023-11-28 18:24:22 -0800

    Uploaded current C file and nasm file for Problem 2

---
+++ homework/homework06/findGCDfunc.asm
@@ -0,0 +1,39 @@
+; Aidan Esposito / Juan Ballesteros
+
+    global findGCD
+    section .text
+
+_main: 
+
+    ;add values to registers
+    mov eax, ecx
+    mov ebx, edx
+
+    ; eax >= ebx, swap if needed
+    cmp eax, ebx
+    jge gcd_loop
+
+    mov ecx, eax
+    mov eax, ebx
+    mov ebx, ecx
+
+gcd_loop:
+
+    ;checks if(ebx == 0 ) and if yes, GCD is found
+    cmp ebx, 0
+    je gcd_done
+
+    ;Euclidean algorithm for gcd
+    mov edx, eax
+    mov eax, ebx
+    xor ebx, ebx
+    div edx
+    mov ebx, eax
+
+    ;loop for gcd
+    jmp gcd_loop
+
+gcd_done:
+
+   ;Exit program
+   ret
---
+++ homework/homework06/gcdFinder.c
@@ -0,0 +1,32 @@
+//Aidan Esposito / Juan Ballesteros
+
+#include <stdio.h>
+#include <stdint.h>  
+#include <assert.h>
+
+//external GCD function from findGCDfunc.asm
+extern int32_t findGCD(int32_t a, int32_t b);
+
+int main(){
+
+    //Numbers to be used to test function
+    int32_t num1, num2;
+
+
+    //Numbers entered from user
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+
+    //value used to keep track of numbers entered
+    int32_t result = findGCD(num1, num2);
+
+    //check if result > 0 
+    assert(result > 0);
+    //check if result == 1
+    assert(result == 1);
+    //print GCD
+    printf("Numbers: %d %d, GCD: %d\n", num1, num2, result);
+
+    return 0;
+}
